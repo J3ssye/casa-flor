a43f140e473ed8568f7ac0dfd34143c12846c0ba
Jessyca <j3ssyca.r.s@gmail.com>
2026-06-30 09:29:14 -0300

    Configura build para producao (prisma generate + db push) e remove migrations obsoletas

--- prisma/migrations/20260619001509_escala_progresso_periodicidade_financeiro/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterEnum
-ALTER TYPE "CategoriaDespesa" ADD VALUE 'ALUGUEL';
-
--- AlterTable
-ALTER TABLE "EscalaItem" ADD COLUMN     "observacaoMoradora" TEXT,
-ADD COLUMN     "percentualConclusao" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "Tarefa" ADD COLUMN     "vezesNaSemana" INTEGER;
